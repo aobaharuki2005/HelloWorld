ee242c3a5ea598b93fce03ad117a1f632c4110b4
baotg235019-soict <bao.tg235019@sis.hust.edu.vn>
2025-03-18 12:07:43 +0700

    Tat ham HelloWorld, them ham in modulus

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,14 @@
 # in ra man hinh dong chu HelloWorld
-print("Hello World")
+# print("Hello World")
 
 # in ra tong cua 2 so
 def printsum(a, b):
     print(f"Tong cua {a} va {b} la {a + b}")
 # Chay thu
 printsum(2, 5)
+
+# in ra so du cua phep chia a cho b
+def printmod(a, b):
+    print(f"So du cua {a} khi chia cho {b} la {a % b}")
+# Chay thu
+printmod(14, 3)
